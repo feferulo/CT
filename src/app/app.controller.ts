1c7a69417298296cf62c69643525e54dbcf19842
feferulo <fede.carra@gmail.com>
2022-04-24 11:42:31 -0300

    Serve swagger doc in api/swagger

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,11 +1,14 @@
 import { controller, IAppController } from '@foal/core';
 import { createConnection } from 'typeorm';
 
-import { ApiController } from './controllers';
+import { ApiController, OpenApiController } from './controllers';
+// import { ApiController } from './controllers';
 
-export class AppController implements IAppController {
+export class AppController  implements IAppController {
   subControllers = [
+    // controller('/api', ApiController),
     controller('/api', ApiController),
+    controller('/swagger', OpenApiController)
   ];
 
   async init() {
--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,10 +1,49 @@
 import { Context, Get, HttpResponseOK } from '@foal/core';
+import { ApiInfo, ApiServer, ApiDefineSchema } from '@foal/core';
+import { ApiResponse } from '@foal/core';
 
+@ApiDefineSchema('trader', {
+  type: 'object',
+  properties: {
+    name: { type: 'string' }
+  },
+  required: [ 'name' ],
+})
+@ApiInfo({
+  title: 'CopyToken API',
+  version: '1.0.0'
+})
+@ApiServer({
+  url: '/api'
+})
 export class ApiController {
 
   @Get('/')
+  @ApiResponse(200, { description: 'successful operation' })
   index(ctx: Context) {
     return new HttpResponseOK('Hello world!');
   }
 
+  @Get('/traders')
+  @ApiResponse(200, {
+    description: 'successful operation',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: { $ref: '#/components/schemas/trader' }
+        }
+      }
+    }
+  })
+  @ApiResponse(200, { description: 'successful operation' })
+  getTraders(ctx: Context) {
+    let traders = [{"name": "Monty"}, {"name": "Python"}];
+    return new HttpResponseOK(traders);
+  }
 }
+
+
+
+
+
