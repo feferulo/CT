8610c58dcaabec698ab81c32392f224d16cc6a5c
feferulo <fede.carra@gmail.com>
2022-05-02 16:46:55 -0300

    remove comments (trying IDE)

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -2,11 +2,10 @@ import { controller, IAppController } from '@foal/core';
 import { createConnection } from 'typeorm';
 
 import { ApiController, OpenApiController } from './controllers';
-// import { ApiController } from './controllers';
 
 export class AppController  implements IAppController {
   subControllers = [
-    // controller('/api', ApiController),
+
     controller('/api', ApiController),
     controller('/swagger', OpenApiController)
   ];
